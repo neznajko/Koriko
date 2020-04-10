08159a385a38756ca4aa460944f57df280444949
Dimitar Panayotov <neznajko@yahoo.com>
2020-04-10 03:42:15 +0300

    .. fix som bgz .., yeah!

--- Koriko.cxx
+++ Koriko.cxx
@@ -14,13 +14,13 @@ struct Point {
 	bool operator==(const Point &lft, const Point &ryt)
 	{
 		return (lft.coor[0] == ryt.coor[0] and
-				lft.coor[1] == ryt.coor[1]);
+		        lft.coor[1] == ryt.coor[1]);
 	}
 	friend
 	ostream &operator<<(ostream &out, const Point &p)
 	{
 		out << "(" << p.coor[0] 
-			<< "," << p.coor[1] << ")";
+		    << "," << p.coor[1] << ")";
 
 		return out;
 	}
@@ -28,35 +28,35 @@ struct Point {
 typedef vector<Point> line_t;
 /*                                        global data */
 double SeaLevl[M][N] = {{ +3.5, +1.3, +0.0 },
-						{ +8.7, +5.2, -4.5 },
-						{ -3.1, -2.0, -1.5 },
-						{ +0.1, +3.5, +2.3 }};
+                        { +8.7, +5.2, -4.5 },
+                        { -3.1, -2.0, -1.5 },
+                        { +0.1, +3.5, +2.3 }};
 line_t *gates[M][N];
 vector<line_t *> stk;  /*   shtack  */
 Point A{1, 0};
 Point B{0, 1};
-/*	,	>	:	!	-	*	'	=	+	@	.	h	- */
+/*  ,   >   :   !   -   *   '   =   +   @   .   h   - */
 line_t *variants(int hrz, int vrt)
 // get all possible variants from that point
 {
-    int fset = 1; // wtf? (offset)
-    int i, j;
+	int fset = 1; // wtf? (offset)
+	int i, j;
 	const double levl = SeaLevl[hrz][vrt];
 	line_t *var = new line_t;
 
     // loof around (hrz, vrt) point N, (W, E), S
-    for (i = hrz - 1; i < hrz + 2; i += 1) {
-        fset = !fset;
-        if (i < 0 or i >= M) continue;
-        for (j = vrt - fset; j < vrt + 2; j += 2) {
-            if (j < 0 or j >= N) continue;
+	for (i = hrz - 1; i < hrz + 2; i += 1) {
+		fset = !fset;
+		if (i < 0 or i >= M) continue;
+		for (j = vrt - fset; j < vrt + 2; j += 2) {
+			if (j < 0 or j >= N) continue;
 			if (SeaLevl[i][j] > levl) continue;
 			var->push_back({i, j});
-        }
+		}
 	}
 	return var;
 }
-/*	-	1	>	,	>	<	0	\\	.	/	o	,	. */
+/*  -   1   >   ,   >   <   0   \\  .   /   o   ,   . */
 void set_gates(void)
 {
 	for (int m=0; m<M; m++) {
@@ -65,7 +65,7 @@ void set_gates(void)
 		}
 	}
 }
-/*	.	8	g	-	8	.	e	8	l	-	g	-	z */
+/*  .   8   g   -   8   .   e   8   l   -   g   -   z */
 void walk(const Point &src, line_t *aux) /* tree walk */
 // tree walk
 {
@@ -91,6 +91,7 @@ void walk(const Point &src, line_t *aux) /* tree walk */
 	aux->clear();
 	delete aux;
 }
+////////////////////////////////////////////////////////
 void dunf(line_t vec) // yeeh!
 {
 	stringstream ss;
@@ -99,6 +100,7 @@ void dunf(line_t vec) // yeeh!
 
 	cout << ss.str();
 }
+////_///////////////////////////////////////////////////
 int main(void)
 {
 	set_gates();
